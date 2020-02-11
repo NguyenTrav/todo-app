a4d927aa8d4c6d866f51c7c4db6a88a24f924c56
Travis <NguyenTrav@gmail.com>
2020-02-10 21:36:42 -0500

    Updates

---
+++ precache-manifest.b60fb428781fe8f86ed6df61f5e059f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f01c447f3bdc703876ad",
+    "url": "/todo-app/static/css/main.17621266.chunk.css"
+  },
+  {
+    "revision": "f01c447f3bdc703876ad",
+    "url": "/todo-app/static/js/main.f01c447f.chunk.js"
+  },
+  {
+    "revision": "a46c3e295f5ff88ce5c1",
+    "url": "/todo-app/static/js/1.a46c3e29.chunk.js"
+  },
+  {
+    "revision": "84c007b1203ce2fd8a61",
+    "url": "/todo-app/static/js/runtime~main.84c007b1.js"
+  },
+  {
+    "revision": "df7689c501594f13d1ba857e3ba94704",
+    "url": "/todo-app/index.html"
+  }
+];
